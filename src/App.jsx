70b48dbc85b5ee052f905ae138f6d2ca5af3d03a
Lucas <109859872+iamdevlucas123@users.noreply.github.com>
2025-12-02 22:20:48 -0300

    add CodeMirror 6 on Route Lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./Routes/Home"
+import Lab from "./Routes/lab"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/lab" element={<Lab/>}/>
     </Routes>
   )
 }
